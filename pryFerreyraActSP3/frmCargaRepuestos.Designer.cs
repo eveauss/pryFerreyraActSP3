30d6778725061c4c976b5b04e8f3ec807d49c580
Evelin <evvhopethv@gmail.com>
2025-09-24 13:33:08 -0300

    Diseño de Interfaz

--- pryFerreyraActSP3/frmCargaRepuestos.Designer.cs
+++ pryFerreyraActSP3/frmCargaRepuestos.Designer.cs
@@ -29,30 +29,33 @@
         private void InitializeComponent()
         {
             gbDatos = new GroupBox();
+            txtPrecio = new TextBox();
+            txtDescripcion = new TextBox();
+            txtNumero = new TextBox();
+            lblPrecio = new Label();
+            lblNumero = new Label();
+            lblDesc = new Label();
+            lblOrigen = new Label();
+            lblMarca = new Label();
             cmbMarca = new ComboBox();
-            cmbOrigen = new ComboBox();
-            this.txtNumero = new TextBox();
-            this.txtDescripcion = new TextBox();
-            this.txtPrecio = new TextBox();
             btnAgregar = new Button();
+            cmbOrigen = new ComboBox();
             gbConsulta = new GroupBox();
+            label1 = new Label();
+            btnConsultar = new Button();
+            rbImportado = new RadioButton();
+            lstResultados = new ListBox();
             cmbConsultaMarca = new ComboBox();
             rbNacional = new RadioButton();
-            rbImportado = new RadioButton();
-            btnConsultar = new Button();
-            lstResultados = new ListBox();
-            lblMarca = new Label();
-            lblOrigen = new Label();
-            lblDesc = new Label();
-            lblNumero = new Label();
-            lblPrecio = new Label();
-            label1 = new Label();
             gbDatos.SuspendLayout();
             gbConsulta.SuspendLayout();
             SuspendLayout();
             // 
             // gbDatos
             // 
+            gbDatos.Controls.Add(txtPrecio);
+            gbDatos.Controls.Add(txtDescripcion);
+            gbDatos.Controls.Add(txtNumero);
             gbDatos.Controls.Add(lblPrecio);
             gbDatos.Controls.Add(lblNumero);
             gbDatos.Controls.Add(lblDesc);
@@ -61,15 +64,79 @@
             gbDatos.Controls.Add(cmbMarca);
             gbDatos.Controls.Add(btnAgregar);
             gbDatos.Controls.Add(cmbOrigen);
-            gbDatos.Controls.Add(this.txtNumero);
-            gbDatos.Controls.Add(this.txtDescripcion);
-            gbDatos.Controls.Add(this.txtPrecio);
             gbDatos.Location = new Point(70, 12);
             gbDatos.Name = "gbDatos";
             gbDatos.Size = new Size(523, 216);
             gbDatos.TabIndex = 0;
             gbDatos.TabStop = false;
             gbDatos.Text = "Ingreso de Datos";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(121, 139);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(120, 23);
+            txtPrecio.TabIndex = 15;
+            // 
+            // txtDescripcion
+            // 
+            txtDescripcion.Location = new Point(338, 96);
+            txtDescripcion.Multiline = true;
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(121, 61);
+            txtDescripcion.TabIndex = 14;
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(122, 97);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(120, 23);
+            txtNumero.TabIndex = 13;
+            // 
+            // lblPrecio
+            // 
+            lblPrecio.AutoSize = true;
+            lblPrecio.Location = new Point(66, 142);
+            lblPrecio.Name = "lblPrecio";
+            lblPrecio.Size = new Size(40, 15);
+            lblPrecio.TabIndex = 12;
+            lblPrecio.Text = "Precio";
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.Location = new Point(65, 100);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(51, 15);
+            lblNumero.TabIndex = 11;
+            lblNumero.Text = "Número";
+            // 
+            // lblDesc
+            // 
+            lblDesc.AutoSize = true;
+            lblDesc.Location = new Point(258, 99);
+            lblDesc.Name = "lblDesc";
+            lblDesc.Size = new Size(69, 15);
+            lblDesc.TabIndex = 10;
+            lblDesc.Text = "Descripción";
+            // 
+            // lblOrigen
+            // 
+            lblOrigen.AutoSize = true;
+            lblOrigen.Location = new Point(257, 50);
+            lblOrigen.Name = "lblOrigen";
+            lblOrigen.Size = new Size(43, 15);
+            lblOrigen.TabIndex = 9;
+            lblOrigen.Text = "Origen";
+            // 
+            // lblMarca
+            // 
+            lblMarca.AutoSize = true;
+            lblMarca.Location = new Point(65, 51);
+            lblMarca.Name = "lblMarca";
+            lblMarca.Size = new Size(40, 15);
+            lblMarca.TabIndex = 8;
+            lblMarca.Text = "Marca";
             // 
             // cmbMarca
             // 
@@ -79,36 +146,6 @@
             cmbMarca.Size = new Size(121, 23);
             cmbMarca.TabIndex = 2;
             // 
-            // cmbOrigen
-            // 
-            cmbOrigen.FormattingEnabled = true;
-            cmbOrigen.Location = new Point(338, 44);
-            cmbOrigen.Name = "cmbOrigen";
-            cmbOrigen.Size = new Size(121, 23);
-            cmbOrigen.TabIndex = 3;
-            // 
-            // txtNumero
-            // 
-            this.txtNumero.Location = new Point(121, 95);
-            this.txtNumero.Name = "txtNumero";
-            this.txtNumero.Size = new Size(121, 23);
-            this.txtNumero.TabIndex = 5;
-            // 
-            // txtDescripcion
-            // 
-            this.txtDescripcion.Location = new Point(338, 95);
-            this.txtDescripcion.Multiline = true;
-            this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new Size(121, 66);
-            this.txtDescripcion.TabIndex = 6;
-            // 
-            // txtPrecio
-            // 
-            this.txtPrecio.Location = new Point(121, 138);
-            this.txtPrecio.Name = "txtPrecio";
-            this.txtPrecio.Size = new Size(121, 23);
-            this.txtPrecio.TabIndex = 7;
-            // 
             // btnAgregar
             // 
             btnAgregar.Location = new Point(383, 178);
@@ -117,7 +154,14 @@
             btnAgregar.TabIndex = 1;
             btnAgregar.Text = "Agregar";
             btnAgregar.UseVisualStyleBackColor = true;
-            btnAgregar.Click += this.btnAgregar_Click;
+            // 
+            // cmbOrigen
+            // 
+            cmbOrigen.FormattingEnabled = true;
+            cmbOrigen.Location = new Point(338, 44);
+            cmbOrigen.Name = "cmbOrigen";
+            cmbOrigen.Size = new Size(121, 23);
+            cmbOrigen.TabIndex = 3;
             // 
             // gbConsulta
             // 
@@ -134,6 +178,44 @@
             gbConsulta.TabStop = false;
             gbConsulta.Text = "Consulta";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(60, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 13;
+            label1.Text = "Marca";
+            // 
+            // btnConsultar
+            // 
+            btnConsultar.Location = new Point(392, 157);
+            btnConsultar.Name = "btnConsultar";
+            btnConsultar.Size = new Size(75, 23);
+            btnConsultar.TabIndex = 8;
+            btnConsultar.Text = "Consultar";
+            btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // rbImportado
+            // 
+            rbImportado.AutoSize = true;
+            rbImportado.Location = new Point(368, 44);
+            rbImportado.Name = "rbImportado";
+            rbImportado.Size = new Size(81, 19);
+            rbImportado.TabIndex = 9;
+            rbImportado.TabStop = true;
+            rbImportado.Text = "importado";
+            rbImportado.UseVisualStyleBackColor = true;
+            // 
+            // lstResultados
+            // 
+            lstResultados.FormattingEnabled = true;
+            lstResultados.ItemHeight = 15;
+            lstResultados.Location = new Point(56, 87);
+            lstResultados.Name = "lstResultados";
+            lstResultados.Size = new Size(411, 64);
+            lstResultados.TabIndex = 8;
+            // 
             // cmbConsultaMarca
             // 
             cmbConsultaMarca.FormattingEnabled = true;
@@ -145,96 +227,13 @@
             // rbNacional
             // 
             rbNacional.AutoSize = true;
-            rbNacional.Location = new Point(246, 43);
+            rbNacional.Location = new Point(265, 43);
             rbNacional.Name = "rbNacional";
             rbNacional.Size = new Size(72, 19);
             rbNacional.TabIndex = 8;
             rbNacional.TabStop = true;
             rbNacional.Text = "Nacional";
             rbNacional.UseVisualStyleBackColor = true;
-            // 
-            // rbImportado
-            // 
-            rbImportado.AutoSize = true;
-            rbImportado.Location = new Point(368, 43);
-            rbImportado.Name = "rbImportado";
-            rbImportado.Size = new Size(81, 19);
-            rbImportado.TabIndex = 9;
-            rbImportado.TabStop = true;
-            rbImportado.Text = "importado";
-            rbImportado.UseVisualStyleBackColor = true;
-            // 
-            // btnConsultar
-            // 
-            btnConsultar.Location = new Point(392, 157);
-            btnConsultar.Name = "btnConsultar";
-            btnConsultar.Size = new Size(75, 23);
-            btnConsultar.TabIndex = 8;
-            btnConsultar.Text = "Consultar";
-            btnConsultar.UseVisualStyleBackColor = true;
-            // 
-            // lstResultados
-            // 
-            lstResultados.FormattingEnabled = true;
-            lstResultados.ItemHeight = 15;
-            lstResultados.Location = new Point(56, 87);
-            lstResultados.Name = "lstResultados";
-            lstResultados.Size = new Size(411, 64);
-            lstResultados.TabIndex = 8;
-            // 
-            // lblMarca
-            // 
-            lblMarca.AutoSize = true;
-            lblMarca.Location = new Point(65, 51);
-            lblMarca.Name = "lblMarca";
-            lblMarca.Size = new Size(40, 15);
-            lblMarca.TabIndex = 8;
-            lblMarca.Text = "Marca";
-            // 
-            // lblOrigen
-            // 
-            lblOrigen.AutoSize = true;
-            lblOrigen.Location = new Point(257, 50);
-            lblOrigen.Name = "lblOrigen";
-            lblOrigen.Size = new Size(43, 15);
-            lblOrigen.TabIndex = 9;
-            lblOrigen.Text = "Origen";
-            // 
-            // lblDesc
-            // 
-            lblDesc.AutoSize = true;
-            lblDesc.Location = new Point(258, 99);
-            lblDesc.Name = "lblDesc";
-            lblDesc.Size = new Size(69, 15);
-            lblDesc.TabIndex = 10;
-            lblDesc.Text = "Descripción";
-            // 
-            // lblNumero
-            // 
-            lblNumero.AutoSize = true;
-            lblNumero.Location = new Point(65, 100);
-            lblNumero.Name = "lblNumero";
-            lblNumero.Size = new Size(51, 15);
-            lblNumero.TabIndex = 11;
-            lblNumero.Text = "Número";
-            // 
-            // lblPrecio
-            // 
-            lblPrecio.AutoSize = true;
-            lblPrecio.Location = new Point(66, 142);
-            lblPrecio.Name = "lblPrecio";
-            lblPrecio.Size = new Size(40, 15);
-            lblPrecio.TabIndex = 12;
-            lblPrecio.Text = "Precio";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(60, 47);
-            label1.Name = "label1";
-            label1.Size = new Size(40, 15);
-            label1.TabIndex = 13;
-            label1.Text = "Marca";
             // 
             // frmCargaRepuestos
             // 
@@ -258,9 +257,9 @@
         private ComboBox cmbMarca;
         private ComboBox cmbOrigen;
         private MaskedTextBox maskedTextBox1;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
+        //private TextBox textBox1;
+        //private TextBox textBox2;
+        //private TextBox textBox3;
         private Button btnAgregar;
         private GroupBox gbConsulta;
         private RadioButton rbImportado;
@@ -274,5 +273,8 @@
         private Label lblNumero;
         private Label lblPrecio;
         private Label label1;
+        private TextBox txtPrecio;
+        private TextBox txtDescripcion;
+        private TextBox txtNumero;
     }
 }
--- pryFerreyraActSP3/frmCargaRepuestos.cs
+++ pryFerreyraActSP3/frmCargaRepuestos.cs
@@ -7,18 +7,13 @@ namespace pryFerreyraActSP3
             InitializeComponent();
         }
 
-        Repuesto[] repuestos = new Repuesto[100];
-        int CantidadRepuestos = 0;
+      
 
 
 
         private void label2_Click(object sender, EventArgs e)
         {
-            if (CantidadRepuestos >= 100)
-            {
-                MessageBox.Show("Se alcanzó el número de 100 repuestos");
-                return;
-            }
+            
         }
     }
 }
